9f91e28cb496f3f09bb9dbd7eea192b4a39cfb03
Ivan Baluta <ltlaitoff@gmail.com>
2022-01-01 23:00:41 +0200

    feat(functional): add asynchronic call all updates

--- functional/update_all_users_matches.py
+++ functional/update_all_users_matches.py
@@ -6,10 +6,18 @@ from functional.update_user_mathes import update_user_mathes
 async def update_all_users_matches(chat_id):
     all_users = database.users.get_all()
 
-    result = '[ALL_UPDATE] Matches(date, champion, KDA):\n'
+    result_arr = []
     for user in all_users:
+        result_arr.append(await update_user_mathes(user.get("tg_id"), chat_id, 'all'))
+
+    if (len(result_arr) == 0):
+        return
+
+    result = '[ALL_UPDATE] Matches(date, champion, KDA):\n'
+
+    for item in result_arr:
         result += f'{user.get("tg_name")}:\n'
-        result += await update_user_mathes(user.get("tg_id"), chat_id, 'all')
+        result += item
         result += '\n\n'
 
     await dp.bot.send_message(chat_id, result)

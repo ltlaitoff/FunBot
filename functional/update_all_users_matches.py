ee066a322d140a1736f366d38ff063628881017b
Ivan Baluta <ltlaitoff@gmail.com>
2022-01-02 00:00:43 +0200

    fix(functional): fix bug in update_all_users_matches

--- functional/update_all_users_matches.py
+++ functional/update_all_users_matches.py
@@ -12,7 +12,7 @@ async def update_all_users_matches(chat_id, call_type="user"):
         result_arr_names.append(user.get("tg_name"))
         result_arr_text.append(await update_user_mathes(user.get("tg_id"), chat_id, 'all'))
 
-    if (len(result_arr_text) == 0 and call_type != "user"):
+    if (check_array_on_void(result_arr_text) == 0 and call_type != "user"):
         return
 
     result = '[ALL_UPDATE] Matches(date, champion, KDA):\n'
@@ -23,3 +23,11 @@ async def update_all_users_matches(chat_id, call_type="user"):
         result += '\n\n'
 
     await dp.bot.send_message(chat_id, result)
+
+
+def check_array_on_void(arr):
+    for i in arr:
+        if i != '':
+            return True
+
+    return False

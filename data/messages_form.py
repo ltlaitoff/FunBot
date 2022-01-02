ac3028bd3c63ccf18b807b8ba65995871a4710f0
Ivan Baluta <ltlaitoff@gmail.com>
2022-01-02 12:26:51 +0200

    feat: create get_history_message

--- data/messages_form.py
+++ data/messages_form.py
@@ -18,3 +18,23 @@ def get_help_message():
             "/version - Текущая версия бота"
             )
     return "\n".join(text)
+
+
+def get_history_message(history_list):
+    def __create_history_string(item):
+        value = item.get("value")
+        coef = item.get("current_coef")
+        result_pull_ups = item.get("result_pull_ups")
+        global_pull_ups = item.get("global_pull_ups")
+        return f'{item.get("date")} | {value} * {coef} = {global_pull_ups - value * coef} + {result_pull_ups} = {global_pull_ups}'
+
+    if (len(history_list) == 0):
+        return 'История пустая'
+
+    message = 'История:\n'
+    message += 'Тип | Дата | Смерти * coef + pull_ups_old = pull_ups\n'
+
+    for item in history_list[:10]:
+        message += __create_history_string(item) + '\n'
+
+    return message
--- functional/history.py
+++ functional/history.py
@@ -1,6 +1,8 @@
 from loader import database, logger, dp, api
 from datetime import datetime
 
+from data.messages_form import get_history_message
+
 
 def history(tg_id):
     user_info = database.users.get_by_tg_id(tg_id)
@@ -11,13 +13,4 @@ def history(tg_id):
     if (len(history_list) == 0):
         return 'История пустая'
 
-    message = 'История:\n'
-    message += 'Дата | Значение | Коофициент | В итоге | Общее\n'
-    for item in history_list[:10]:
-        message += create_history_string(item) + '\n'
-
-    return message
-
-
-def create_history_string(item):
-    return f'{item.get("date")} | {item.get("value")} | {item.get("current_coef")} | {item.get("result_pull_ups")} | {item.get("global_pull_ups")}'
+    return get_history_message(history_list[:10])

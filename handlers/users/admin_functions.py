a3c2a2c20ac1b658681ca3b37c8b240d58877190
Ivan Baluta <ltlaitoff@gmail.com>
2022-08-23 17:25:31 +0300

    feat(admin commands): Added admin command for check message info

--- handlers/users/admin_functions.py
+++ handlers/users/admin_functions.py
@@ -9,8 +9,9 @@ from filters import IsAdmin
 @dp.message_handler(IsAdmin(), Command("admin_commands"))
 async def update_matches(message: types.Message):
     await message.reply('Admin commands:\n' +
-                        '/send_changes Отправить сообщение об изменениях\n' +
-                        '/database_backup - Скачать бэкап базы данных')
+                        '/send_changes - Отправить сообщение об изменениях\n' +
+                        '/database_backup - Скачать бэкап базы данных\n' + 
+                        '/message_info - Информация о сообщении')
 
 
 @logger.catch
@@ -27,3 +28,8 @@ async def update_matches(message: types.Message):
     with open('utils/db_api/db.db', 'rb') as database_file:
         contents = database_file.read()
         await bot.send_document(config.CHAT, contents, caption='database')
+
+@logger.catch
+@dp.message_handler(IsAdmin(), Command("message_info"))
+async def update_matches(message: types.Message):
+    await message.reply(message)
